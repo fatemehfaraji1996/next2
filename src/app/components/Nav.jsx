1b3133f20c7b38f3a9f76c891e686e2297cc1df5
fatemehfaraji1996 <farajifateme1996@gmail.com>
2024-08-07 09:05:56 +0330

    activstyle

--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -1,26 +1,35 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function Nav() {
+  const path = usePathname();
+
+  const paths = [
+    { title: "home", rout: "/home" },
+    { title: "about", rout: "/about" },
+    { title: "store", rout: "/store" },
+    { title: "support", rout: "/support" },
+  ];
+
   return (
     <div className=" text-cyan-500 m-10 bg-slate-300">
-    <ul>
-    <li>
-      <Link href="/home">Home</Link>
-    </li>
-    <li>
-      <Link href="/about">About Us</Link>
-    </li>
-    <li>
-      <Link href="/store">Store</Link>
-    </li>
-    <li>
-      <Link href="/support">Support</Link>
-    </li>
-  </ul>
-
+      <ul className='flex flex-row gap-7 p-4'>
+        {paths.map((item, i) => {
+          const activeStyle = { color: path === item.rout ? "darkgray" : "#fff" };
+          return (
+            <li>
+              <Link
+                style={{ ...activeStyle, textDecoration: "none" }}
+                href={item.rout}
+              >
+                {item.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-
-    
-  )
+  );
 }
